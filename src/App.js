2ce5cb2e7be2d6ef7f6a836cc996398d36f4b980
zhangqinglong <zhangqinglong@hylink.com>
2023-01-12 18:37:27 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, createRef } from 'react'
+import './App.css'
+import Table from 'react-bootstrap/Table'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [productImage, setProductImage] = useState()
+	const [showUploadProductImage, setShowUploadProductImage] = useState(true)
+	const [productSpecsImage, setproductSpecsImage] = useState()
+	const [showUploadProductSpecsImage, setshowUploadProductSpecsImage] =
+		useState(true)
+
+	const ref = createRef(null)
+
+	const params = [
+		['FRAME MATERIAL', 'PC'],
+		['LENS MATERIAL', 'AC'],
+		['GENDER', 'UNISEX'],
+		['STYLE', 'FASHION'],
+		['WEIGHT', '54G'],
+	]
+
+	const handleChangeProductImage = (e) => {
+		setProductImage(URL.createObjectURL(e.target.files[0]))
+		setShowUploadProductImage(false)
+	}
+	const handleChangeProductSpecsImage = (e) => {
+		setproductSpecsImage(URL.createObjectURL(e.target.files[0]))
+		setshowUploadProductSpecsImage(false)
+	}
+
+	return (
+		<div className="App">
+			<div id="product-specs" ref={ref}>
+				<header className="product-header">PRODUCT PARAMETERS</header>
+				<div className="product">
+					<div className="product-image-container">
+						<label htmlFor="product-image">
+							<input
+								style={{
+									display: showUploadProductImage
+										? 'block'
+										: 'none',
+								}}
+								id="product-image"
+								name="product-image"
+								className="product-image"
+								onChange={handleChangeProductImage}
+								type="file"
+							/>
+							<img src={productImage} />
+						</label>
+					</div>
+					<div className="product-details-container">
+						<Table striped hover variant="dark">
+							<tbody>
+								{params.map((p, index) => (
+									<tr key={index}>
+										<td>{p[0]}</td>
+										<td>{p[1]}</td>
+									</tr>
+								))}
+							</tbody>
+						</Table>
+					</div>
+				</div>
+				<div className="product-specs-container">
+					<label htmlFor="product-specs-image">
+						<input
+							style={{
+								display: showUploadProductSpecsImage
+									? 'block'
+									: 'none',
+							}}
+							id="product-specs-image"
+							name="product-specs-image"
+							className="product-specs-image"
+							onChange={handleChangeProductSpecsImage}
+							type="file"
+						/>
+						<img src={productSpecsImage} />
+					</label>
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
